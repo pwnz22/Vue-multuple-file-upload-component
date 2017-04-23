402a77ada7a3d67b6e376a345d2e08f3427568ef
Valijon <parliament888@gmail.com>
2017-04-23 10:32:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <upload-form></upload-form>
+  </div>
+</template>
+
+<script>
+  import UploadForm from './components/UploadForm'
+
+  export default {
+    name: 'app',
+    components: {
+      UploadForm
+    }
+  }
+</script>
