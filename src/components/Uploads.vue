7d38ae6b52c69d945ec617b46bb17654a6d4f5a9
Valijon <parliament888@gmail.com>
2017-04-30 10:37:42 +0800

    Overall percentage progress

--- src/components/Uploads.vue
+++ src/components/Uploads.vue
@@ -3,7 +3,7 @@
         <div class="dragndrop__status" v-if="files.length">
             <ul class="list-inline">
                 <li class="list-inline__item">Files: {{ files.length }}</li>
-                <li class="list-inline__item">Percentage: x%</li>
+                <li class="list-inline__item">Percentage: {{ overallProgress }}%</li>
                 <li class="list-inline__item list-inline__item--last">Time remaining: x%</li>
             </ul>
         </div>
@@ -13,9 +13,45 @@
 
 <script>
     import File from './File'
+    import eventHub from '../events'
+
     export default {
+        data () {
+            return {
+                overallProgress: 0
+            }
+        },
         props: ['files'],
-        components: {File}
+        components: {File},
+        methods: {
+            unFinishedFiles() {
+                let i, files = []
+
+                for(i = 0; i < this.files.length; i++) {
+                    if (this.files[i].finished || this.files[i].cancelled) {
+                        //
+                    }
+                    files.push(this.files[i])
+                }
+
+                return files
+            },
+            updateOverallProgress() {
+                let unfinishedFiles = this.unFinishedFiles(), totalProgress = 0
+                unfinishedFiles.forEach(file => {
+                    totalProgress += file.progress
+                })
+
+                this.overallProgress = parseInt(totalProgress / unfinishedFiles.length || 0)
+
+                console.log(totalProgress)
+            }
+        },
+        mounted() {
+            eventHub.$on('progress', (fileObject, e) => {
+                this.updateOverallProgress()
+            })
+        }
     }
 </script>
 
